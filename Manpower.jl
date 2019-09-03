360ef32741caadc285e42318db77372ed9886924
Frederik-Nygaard <frederiknygaard@frederiks-mbp-4.eduroam.wireless.dtu.dk>
2019-09-03 17:16:04 +0200

    Added support for question 4 and 5 to Manpower

--- Manpower.jl
+++ Manpower.jl
@@ -11,15 +11,13 @@ m = [
 1 1 1 1 0 0 1;
 ]
 e = [10 5 10 5 10 5 10]
+p = [1 2 3 4 5 6 7] #Add a price vector
 D = size(e,2)
+k = zeros(Float64, D,D)
 Manpower = Model(with_optimizer(GLPK.Optimizer))
 @variable(Manpower, x[1:D] >= 0, Int)
-@objective(Manpower, Min, sum(x[d] for d=1:D))
-
-
+@objective(Manpower, Min, sum(x[d]*p[d] for d=1:D)) #objevtive changed to minimize total payout
 @constraint(Manpower, [d2=1:D], sum(x[d]*m[d,d2] for d=1:7) >= e[d2])
-
-
 solution = optimize!(Manpower)
 
 println("RESULTS:")
@@ -28,3 +26,11 @@ println("Procurement planning result: $(objective_value(Manpower))")
 for i in 1:7
     println("X1:" , JuMP.value(x[i]))
 end
+
+# calculates the total number of worked hours, and substracts the required hours
+for i in 1:D
+    for j in 1:D
+        k[i,j] = sum(JuMP.value(x[i])*m[i,j])
+    end
+end
+ overwork = sum(k)-sum(e)
